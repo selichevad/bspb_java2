42221436c750a59a07bb3ab0429ed3e0b07d5981
selichevad <al.selichev@gmail.com>
2021-09-22 23:19:57 +0300

    Lesson3 homework

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -18,12 +18,15 @@ public class Lesson3 {
             //перевод строк в массивы символов
             char[] char1 = s1.toCharArray();
             char[] char2 = s2.toCharArray();
+
             //сортировка массивов по символам
             Arrays.sort(char1);
             Arrays.sort(char2);
+
             //обратный перевод массивов в строки
             s1 = String.valueOf(char1);
             s2 = String.valueOf(char2);
+
             //сравнение новых строк с отсортированными символами
             if (s1.equals(s2)) {
                 System.out.println("Строки \"" + str1 + "\" и \"" + str2 + "\" являются анаграммой");
